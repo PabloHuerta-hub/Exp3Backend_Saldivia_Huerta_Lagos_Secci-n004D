baa9dff95e0429eab3d97e463355bc71be30653f
Pablo <pa.huertam@duocuc.cl>
2021-06-15 15:50:40 -0400

    borrado

--- Exp3Backend_Saldivia_Huerta_Lagos_Sección004D/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import inicio,cartelera,sobrenosotros
-
-urlpatterns =[
-    path('',inicio,name="Inicio"),
-    path('cartelera',cartelera,name="Galeria"),
-    path('sobre nosotros',sobrenosotros,name="QuienesSomos")
-]
